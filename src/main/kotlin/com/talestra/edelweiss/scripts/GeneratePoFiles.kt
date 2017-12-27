fe7f48c165156eac04bd8a7ed440006b83990a05
soywiz <soywiz@gmail.com>
2017-12-27 11:51:34 +0100

    Reorganizaed things

---
+++ src/main/kotlin/com/talestra/edelweiss/scripts/GeneratePoFiles.kt
@@ -0,0 +1,9 @@
+package com.talestra.edelweiss.scripts
+
+import com.soywiz.korio.vfs.resourcesVfs
+
+object GeneratePoFiles {
+    @JvmStatic fun main(args: Array<String>) {
+        println("GeneratePoFiles!")
+    }
+}
